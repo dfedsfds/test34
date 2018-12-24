00d800476145c9f091c9fdebc33f9e35f92bf944
dfedsfds <45593336+dfedsfds@users.noreply.github.com>
2018-12-25 00:39:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2655,7 +2655,7 @@ msg.delete();
 });
 
 client.on("message", (message) => {
-if (message.content === prefix + "$ct")) {
+if (message.content === prefix + "$ct") {
             if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
         let args = message.content.split(" ").slice(1);
     message.guild.createChannel(args.join(' '), 'text');
